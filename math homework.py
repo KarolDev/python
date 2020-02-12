5d5226050e87e75a82d61aeecc581f803669435a
KarolDev <lav.lorak.olleh@gmail.com>
2020-02-11 20:22:00 -0600

    problema matemático

---
+++ math homework.py
@@ -0,0 +1,7 @@
+print("math homework.py")
+problema = input("ingresa un problema matemático o 'q' para salir")
+while (problema != "q"):
+    print("La respuesta a ", problema,"es:",eval(problema))
+    problema = input("Ingresa otro prblema matemático o 'q para salir'")
+
+         
